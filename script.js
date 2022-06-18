c3503c25548d398bafca139fbead81c1ae9fb74a
mojeedkusimo <mkusimo90@gmail.com>
2022-06-18 10:24:01 +0100

    Added JavaScript functionality

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-let images = [
+// Listing of available pictures in an array
+
+let imageSources = [
     'benin-ringroad.jpg',
     'lagos-bridge.jpg',
     'lagos-skyline.jpg',
     'national-theatre.jpg'
 ];
 
+// Getting access to button and image element in the HTML document
 let button = document.querySelector("button");
 let imgTag = document.querySelector("img");
+
+//Declaration of a counter variable
 let counter = 1;
 
 button.addEventListener("click", function() {
     if ( counter === 4 ) {
         counter = 0;
     }
-    imgTag.src="./img/" + images[counter];
+    imgTag.src="./img/" + imageSources[counter];
     counter = counter + 1;
 
 });
